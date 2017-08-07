372ec4458d6d3fec4918f79c09f9e21164ddbfa4
Liovan <li0van@yahoo.com>
2017-08-07 12:16:22 +0430

    everything seems fine

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.8.2"
+lock "3.9.0"
 
 set :application, "m"
 set :repo_url, "git@gitlab.com:malekmfs/sample_app.git"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,19 +60,19 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
   #TODO config mail with sendgrid
-  config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    host = '<your heroku app>.herokuapp.com'
-    config.action_mailer.default_url_options = { host: host }
-    ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
-    }
+  # config.action_mailer.raise_delivery_errors = true
+  #   config.action_mailer.delivery_method = :smtp
+  #   host = '<your heroku app>.herokuapp.com'
+  #   config.action_mailer.default_url_options = { host: host }
+  #   ActionMailer::Base.smtp_settings = {
+  #     :address        => 'smtp.sendgrid.net',
+  #     :port           => '587',
+  #     :authentication => :plain,
+  #     :user_name      => ENV['SENDGRID_USERNAME'],
+  #     :password       => ENV['SENDGRID_PASSWORD'],
+  #     :domain         => 'heroku.com',
+  #     :enable_starttls_auto => true
+  #   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
