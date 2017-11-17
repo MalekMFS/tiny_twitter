c29f226a6f9faa243ab8a9e0dda370c3f88c1024
Liovan <li0van@yahoo.com>
2017-11-17 21:13:30 +0330

    email config url fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
   # config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { host: "noreply@meam.ir" }
+    config.action_mailer.default_url_options = { host: "social.meam.ir" }
     ActionMailer::Base.smtp_settings = {
       :address        => 'mail.meam.ir',
       :port           => '587',
