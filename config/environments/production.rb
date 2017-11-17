112ccdbcf8a5c2f8ad0dd7b609ea8bd5cb63b3e8
Liovan <li0van@yahoo.com>
2017-11-17 20:59:21 +0330

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,20 +59,19 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  #TODO config mail with sendgrid
   # config.action_mailer.raise_delivery_errors = true
-  #   config.action_mailer.delivery_method = :smtp
-  #   host = '<your heroku app>.herokuapp.com'
-  #   config.action_mailer.default_url_options = { host: host }
-  #   ActionMailer::Base.smtp_settings = {
-  #     :address        => 'smtp.sendgrid.net',
-  #     :port           => '587',
-  #     :authentication => :plain,
-  #     :user_name      => ENV['SENDGRID_USERNAME'],
-  #     :password       => ENV['SENDGRID_PASSWORD'],
-  #     :domain         => 'heroku.com',
-  #     :enable_starttls_auto => true
-  #   }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { host: "noreply@meam.ir" }
+    ActionMailer::Base.smtp_settings = {
+      :address        => 'mail.meam.ir',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => "noreply@meam.ir",
+      :password       => "PPWBHS9m2d",
+      # => :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+      openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
+    }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
